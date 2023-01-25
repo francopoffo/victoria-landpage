8b765873a1fe0ae2bc7fc4c05712c71ba74789e8
Franco Poffo <franco_sutter@yahoo.com.br>
2023-01-25 11:19:46 -0300

    Melhorando SEO de acordo com instruções da Lighthouse

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/reset.css";
 import "../styles/globals.css";
 import { Inter } from "@next/font/google";
 import Layout from "../src/components/Layout/Layout";
+import Head from "next/head";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -10,9 +11,17 @@ const inter = Inter({
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout className={inter.className}>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Victoria Werner - Advocacia Criminal</title>
+      </Head>
+      <Layout className={inter.className}>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
