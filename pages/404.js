f6758aded58c7f1e1ac94835b03ad46daedf96c1
Franco Poffo <franco_sutter@yahoo.com.br>
2025-02-23 22:03:41 -0300

    total remake of the website

--- pages/404.js
+++
@@ -1,5 +0,0 @@
-import ErrorPage from "../src/components/ErrorPages/ErrorPage";
-
-export default function Custom404() {
-  return <ErrorPage error={"404 error - Page not found"} />;
-}
--- pages/500.js
+++
@@ -1,5 +0,0 @@
-import ErrorPage from "../src/components/ErrorPages/ErrorPage";
-
-export default function Custom505() {
-  return <ErrorPage error={"500 error - Server-side error occurred"} />;
-}
